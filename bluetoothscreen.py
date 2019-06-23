a0fd81fd6741f7741fd124c228e17e70808012c3
karl oficina <karl@aspire.com>
2019-06-23 16:39:20 -0400

    BluetoothScreen se puede echar a andar por si solo

--- bluetoothscreen.py
+++ bluetoothscreen.py
@@ -4,8 +4,9 @@ import threading
 
 from math import sin
 
-from kivy.properties import StringProperty
-from kivy.uix.screenmanager import Screen
+from kivy.properties import StringProperty, ObjectProperty
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.boxlayout import BoxLayout
 
 BLUETOOTH_NAME = 'ESP32test'
 
@@ -17,6 +18,24 @@ if not('BLUETOOTH_OFF' in os.environ):
     UUID = autoclass('java.util.UUID')
 
 gbl_force_close = False
+
+class SetGraph(BoxLayout):
+    graph_test = ObjectProperty(None)
+    def update_graph(self):
+        pass
+    def pintar(self):
+        print("update_graph")
+        self.plot = MeshLinePlot(color=[1, 0, 0, 1])
+        self.plot.points = [(x, sin(x / 10.)) for x in range(0, 20)]
+        self.x = 20
+        print(self.plot)
+        print(self.plot.points)
+        self.ids["graph_test"].add_plot(self.plot)
+        print(self.graph_test)
+        print("CLICK!!!")
+    def add_point(self, _x, _y):
+        self.x = self.x + 1
+        self.plot.points.append((self.x, 2))
 
 
 def get_socket_stream(name):
@@ -63,9 +82,12 @@ class BluetoothScreen(Screen):
     def on_enter(self):
         print("Bienvenido!")
         gbl_force_close = False
-        recv_stream, send_stream = get_socket_stream(BLUETOOTH_NAME)
-        self.send_stream = send_stream
-        threading.Thread(target=working, args=(recv_stream, self.actualizar, self.raw_log, )).start()
+        if not('BLUETOOTH_OFF' in os.environ):
+            recv_stream, send_stream = get_socket_stream(BLUETOOTH_NAME)
+            self.send_stream = send_stream
+            threading.Thread(target=working, args=(recv_stream, self.actualizar, self.raw_log, )).start()
+        else:
+            print("NO BLUETOOTH")
     def raw_log(self, caracter):
         self.raw_txt_str = self.raw_txt_str + caracter
         self.raw_txt = self.raw_txt_str
@@ -76,4 +98,14 @@ class BluetoothScreen(Screen):
         gbl_force_close = True
         print("Cerrando salida")
 
+if __name__ == '__main__':
+    from kivy.app import App
+    class BluetoothScreenApp(App):
+        def on_connect(self):
+            print("Conectando...")
+        def build(self):
+            sm = ScreenManager()
+            sm.add_widget(BluetoothScreen(name='bluetooth'))
+            return sm
+    BluetoothScreenApp().run()
 
