94ec495abb943f50f823931c4dd31b94196f14e9
karl oficina <karl@aspire.com>
2019-06-22 21:25:09 -0400

    crea un thread que funciona en otra funcion

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import NumericProperty
 
+import time
+
+def working():
+    for i in range(10):
+        print("i = %d" % i)
+        time.sleep(5)
+
+
 class Thread(BoxLayout):
     counter = NumericProperty(0)
 
@@ -12,7 +20,7 @@ class Thread(BoxLayout):
         self.ids.lbl.text = "{}".format(self.counter)
 
     def First_thread(self):
-        threading.Thread(target = self.Counter_function).start()
+        threading.Thread(target = working).start()
         self.counter += 1
         self.ids.lbl.text = "{}".format(self.counter)
 
